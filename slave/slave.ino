501fa2657e734f6d5fad961fa153f743036c15da
lavakalas <lavakalas@mail.ru>
2024-11-25 02:19:30 +0300

    Added client(slave)

--- slave/slave.ino
+++ slave/slave.ino
@@ -5,12 +5,12 @@
 
 const char* ssid = "ESP32-Access-Point";
 const char* password = "123456789";
-
-const char* serverPath = "http://192.168.4.1/wonderfulnessie";
+String message = "YAEBALVROTETOTVASHREST";
+String serverPath = "http://192.168.4.1/wonderfulnessie?message=" + message;
 
 unsigned long lastTime = 0;
 
-unsigned long timerDelay = 5000;
+unsigned long timerDelay = 500;
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,5 +28,18 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if ((millis() - lastTime) > timerDelay){
+    if (WiFi.status() == WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
 
+      http.begin(client, serverPath);
+
+      String httpRequestData = "";
+      int httpResponseCode = http.POST(httpRequestData);
+      Serial.println(httpResponseCode);
+      http.end();
+    }
+  }
+  lastTime = millis();
 }
